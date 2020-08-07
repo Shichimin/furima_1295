8cf563d041ccc0ca6c7fbc82c23de98edd49af84
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-08-07 20:13:00 +0900

    バリデーションのリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,29 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
-  validates :birthday, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :email,    format: { with: /\A\S+@\S+\.\S+\z/ },
+                         uniqueness: true
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i },
+                         length: { minimum: 6 },
+                         confirmation: true
 
-  validates :email, uniqueness: true
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+      validates :family_name
+      validates :first_name
+    end
 
-  validates :password, length: { minimum: 6 }
-
-  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
-
-  validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-
-  validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-
-  validates :email, format: { with: /\A\S+@\S+\.\S+\z/ }
-
-  validates :password, confirmation: true
-
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :family_name_kana
+      validates :first_name_kana
+    end
+  end
 end
