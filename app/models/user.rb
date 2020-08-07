d167ffe33d1a3cb6f3cdfb1d5a4ba96f1367dfc1
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-08-07 19:30:05 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,30 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :birthday, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+
+  validates :email, uniqueness: true
+
+  validates :password, length: { minimum: 6 }
+
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
+
+  validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+
+  validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+
+  validates :email, format: { with: /\A\S+@\S+\.\S+\z/ }
+
+  validates :password, confirmation: true
+
 end
