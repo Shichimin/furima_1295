50dea372abfa452815b0c7fab24b18777c952ca3
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-08-09 14:29:25 +0900

    ActiveHash::Baseクラスを継承したCategoryクラスを作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+
   belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :image
+    validates :description
+  end
+
+  validates :category_id, numericality: { other_than: 1 }
 end
