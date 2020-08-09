06b9382844f145b1002c0fab0516c08e75093761
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-08-09 15:20:09 +0900

    ItemモデルとShippingOriginモデルのアソシエーションを定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,21 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
+  belongs_to_active_hash :shipping_origin
 
   belongs_to :user
 
   with_options presence: true do
     validates :name
     validates :image
+    validates :price
     validates :description
+    validates :category_id
+    validates :shipping_origin_id
   end
 
-  validates :category_id, numericality: { other_than: 1 }
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :shipping_origin_id
+  end
 end
