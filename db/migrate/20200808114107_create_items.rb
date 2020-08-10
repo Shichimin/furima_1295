60d3baf740af18c6d87b9c335228c7d6b2800f54
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-08-10 12:33:55 +0900

    itemsテーブルに制約を追加

--- db/migrate/20200808114107_create_items.rb
+++ db/migrate/20200808114107_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string  :name
-      t.text    :image
-      t.text    :description
-      t.integer :price
-      t.integer :category_id
-      t.integer :shipping_origin_id
-      t.integer :condition_id
-      t.integer :shipping_burden_id
-      t.integer :estimated_shipping_date_id
+      t.string  :name,                       null: false, default: ""
+      t.text    :image,                      null: false, default: ""
+      t.text    :description,                null: false, default: ""
+      t.integer :price,                      null: false, default: ""
+      t.integer :category_id,                null: false, default: ""
+      t.integer :shipping_origin_id,         null: false, default: ""
+      t.integer :condition_id,               null: false, default: ""
+      t.integer :shipping_burden_id,         null: false, default: ""
+      t.integer :estimated_shipping_date_id, null: false, default: ""
       t.integer :user_id, foreign_key: true
       t.timestamps
     end
