131a1d90f35e0724902389ebfefe87b069d6e906
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-08-12 02:16:13 +0900

    ShippingAddressにバリデーションを設定

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -13,6 +13,12 @@ class ShippingAddress < ApplicationRecord
     validates :phone_number
   end
 
-  #都道府県の選択が「--」のときは保存できないようにする
+  # 都道府県の選択が「--」のときは保存できないようにする
   validates :prefecture_id, numericality: { other_than: 1 } 
+
+  # 電話番号は11桁以内
+  validates :phone_number, length: { maximum: 11 }
+
+  # 郵便番号は必ずハイフンを含む
+  validates :zip_code, format: { with: /[^\d\-]/ }
 end
