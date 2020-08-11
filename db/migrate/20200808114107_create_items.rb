fc276c0d08a6ec61095824b9ad8c965fbb2f4dbb
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-08-12 01:36:54 +0900

    デバッグ

--- db/migrate/20200808114107_create_items.rb
+++ db/migrate/20200808114107_create_items.rb
@@ -2,14 +2,13 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
       t.string  :name,                       null: false, default: ""
-      t.text    :image,                      null: false, default: ""
-      t.text    :description,                null: false, default: ""
-      t.integer :price,                      null: false, default: ""
-      t.integer :category_id,                null: false, default: ""
-      t.integer :shipping_origin_id,         null: false, default: ""
-      t.integer :condition_id,               null: false, default: ""
-      t.integer :shipping_burden_id,         null: false, default: ""
-      t.integer :estimated_shipping_date_id, null: false, default: ""
+      t.text    :description,                null: false
+      t.integer :price,                      null: false
+      t.integer :category_id,                null: false
+      t.integer :shipping_origin_id,         null: false
+      t.integer :condition_id,               null: false
+      t.integer :shipping_burden_id,         null: false
+      t.integer :estimated_shipping_date_id, null: false
       t.integer :user_id, foreign_key: true
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -34,21 +34,22 @@ ActiveRecord::Schema.define(version: 2020_08_11_113705) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.text "image"
-    t.text "description"
-    t.integer "price"
-    t.integer "category_id"
-    t.integer "shipping_origin_id"
-    t.integer "condition_id"
-    t.integer "shipping_burden_id"
-    t.integer "estimated_shipping_date_id"
+    t.string "name", default: "", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.integer "category_id", null: false
+    t.integer "shipping_origin_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "shipping_burden_id", null: false
+    t.integer "estimated_shipping_date_id", null: false
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "purchases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "item_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
