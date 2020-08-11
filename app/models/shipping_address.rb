6c8f15e79219931de300beb8da429151913a7981
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-08-11 20:42:54 +0900

    ShippingAddressモデルにバリデーションを設定

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,3 +1,13 @@
 class ShippingAddress < ApplicationRecord
   belongs_to :item
+
+  # 空の投稿を保存できないようにする
+  with_options presence: true do
+    validates :zip_code
+    validates :prefecture_id
+    validates :city
+    validates :house_number
+    validates :property_name
+    validates :phne_number
+  end
 end
