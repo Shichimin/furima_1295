65c4d1e711cc518bdc94387cba09e728042f2fd5
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-08-12 05:50:58 +0900

    バリデーションの修正

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,4 +1,5 @@
 class ShippingAddress < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 
   belongs_to :item
@@ -9,7 +10,6 @@ class ShippingAddress < ApplicationRecord
     validates :prefecture
     validates :city
     validates :house_number
-    validates :property_name
     validates :phone_number
   end
 
