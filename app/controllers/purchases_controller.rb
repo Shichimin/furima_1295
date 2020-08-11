baac7d232a860d965cdfc64ece142cb1844ce781
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-08-11 21:10:23 +0900

    Purchasesコントローラーにアクションを定義

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,23 @@
 class PurchasesController < ApplicationController
+  def new
+    @purchase = ItemPurchase.new
+  end
+
+  def create
+    @purchase = ItemPurchase.new(purchase_params)
+  end
+
+  private
+
+  def purchase_params
+    params.require(:item_purchase).permit(
+      :zip_code,
+      :prefecture_id,
+      :city,
+      :house_number,
+      :property_name,
+      :phone_number,
+      :user_id,
+      :item_id)
+  end
 end
