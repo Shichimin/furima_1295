6fd2709a07446962e4e6ee7cb3f9fc5d4ea02d83
Shichimin <35617411+Shichimin@users.noreply.github.com>
2020-08-12 16:41:21 +0900

    Basic認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   protected
 
@@ -12,5 +13,13 @@ class ApplicationController < ActionController::Base
                                                        :family_name_kana,
                                                        :first_name_kana,
                                                        :birthday])
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
